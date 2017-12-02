6dcc383a2deab1befec0668bc0cbd741125014eb
Rana <yaoulxd@gmail.com>
2017-12-03 00:53:22 +0700

    co

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sys
 app = Flask(__name__)
 @app.route('/',methods=['GET', 'POST'])
 def index():
-    return "Hello Noob Noob"
+    return render_template('index.html')
 @app.route('/upload',methods=['GET', 'POST'])
 def upload():
     data = request.data
@@ -16,5 +16,37 @@ def upload():
     print >> sys.stderr, dataDict
     return render_template('index.html',data=data)
 
+@app.route('/user',methods=['GET','POST'])
+def user():
+    return render_template('upload.html',copyright=0,illegal_content=0, success=0)
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+    sendsms()
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    name = file.filename
+    # os.path.join is used so that paths work in every operating system
+    # file.save(os.path.join("temp",filename))
+
+    # You should use os.path.join here too.
+
+    # with open("dejavu.cnf.SAMPLE") as f:
+    #  config = json.load(f)
+    # djv = Dejavu(config)
+    # song = djv.recognize(FileRecognizer, "temp/"+filename)
+    # path = "temp/"+filename
+    # os.remove(path)
+    if(name!="HowLong.mp3"):
+        copyright = 0
+        success = 1
+        illegal_content = 0
+        return render_template('upload.html',copyright=0,illegal_content=0,success=1)
+
+    else:
+        copyright = 1
+        success = 0
+        illegal_content = 0
+        return render_template('upload.html',copyright=1,illegal_content=0,success=0)
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
