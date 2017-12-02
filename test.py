24b909fbbe4b497283920ced1f777f5bd0b9190e
Rana <yaoulxd@gmail.com>
2017-12-02 20:10:51 +0700

    noobsessssssss

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ from werkzeug import secure_filename
 import json
 import requests
 import os
+import sys
 app = Flask(__name__)
 @app.route('/upload',methods=['GET', 'POST'])
 def upload():
     data = request.body.destinationAddress
+    print(data,file=sys.stderr)
     return render_template('index.html',data=data)
 
 if __name__ == "__main__":
