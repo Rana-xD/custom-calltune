0b0dd7dd3502a019da87d3cf0153eb889ebbf62c
Rana <yaoulxd@gmail.com>
2017-12-03 13:23:25 +0700

    Unaraaaa

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def upload():
             'Content-type': 'application/json',
     }
     print data
-    r = requests.post("https://api.dialog.lk/sms/send",data=data,headers=headers)
+    r = requests.post("https://api.dialog.lk/sms/send",data=json.dumps(data),headers=headers)
     print "DONE chef!"
 
     resp=r.content
@@ -88,7 +88,7 @@ def sendsms():
             'Content-type': 'application/json',
     }
     print data
-    r = requests.post("https://api.dialog.lk/sms/send",data=data,headers=headers)
+    r = requests.post("https://api.dialog.lk/sms/send",data=json.dumps(data),headers=headers)
     print "DONE chef!"
 
     resp=r.content
