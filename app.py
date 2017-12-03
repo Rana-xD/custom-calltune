6b260f8bfe08621cc4a1ac3ecd20630ae35e530e
Rana <yaoulxd@gmail.com>
2017-12-03 09:31:18 +0700

    Rana

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 @app.route('/upload',methods=['GET', 'POST'])
 def upload():
     data = request.data
